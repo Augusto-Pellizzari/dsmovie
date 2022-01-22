b0b688f1f5d6c2fc7dea2da191fad254741b5296
Augusto-Pellizzari <apluna_op1@hotmail.com>
2022-01-22 18:19:18 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -2,7 +2,11 @@ import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
+type Props = {
+    movie: Movie;
+}
 
+function MovieCard({ movie }: Props) {
 
     return (
         <div>
